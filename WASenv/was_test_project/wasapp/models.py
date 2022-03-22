967ad7653fb480b60c11deaeb52246f2d88c279f
AOtter24 <aotts10@yahoo.co.uk>
2022-03-22 15:58:48 +0000

    Server 'working'

--- WASenv/was_test_project/wasapp/models.py
+++ WASenv/was_test_project/wasapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    date = models.CharField(max_length=128, unique=True)
+    desc = models.CharField(max_length=250, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __list__(self):
+        return [self.title, self.date, self.desc]
--- WASenv/was_test_project/wasapp/views.py
+++ WASenv/was_test_project/wasapp/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from wasapp.models import Category
 
 # Create your views here.
 def course(request):
-    return HttpResponse("If u can see me well done!")
+    course_list = Category.objects.all()
+
+    context_dict = {}
+    context_dict['courses'] = course_list
+    return render(request, 'was_test/was.html', context=context_dict)
